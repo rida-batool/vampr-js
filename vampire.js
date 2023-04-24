cd40e2ccb1cd59071691e204cfae18cf36b58d96
Rida Batool <ridabatool.be13@gmail.com>
2023-04-23 21:34:02 -0400

    vampire lineage creation methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOFVamps = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numOFVamps++;
+    }
+    return numOFVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    let diff = this.numberOfVampiresFromOriginal - vampire.numberOfVampiresFromOriginal;
+    if (diff < 0) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
